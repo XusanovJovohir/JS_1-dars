a841c1293a63db67c7fb67574024154702fa30cc
Xusanov Jovohir <134719096+XusanovJovohir@users.noreply.github.com>
2023-05-27 18:43:12 +0500

    JS 1-dars
    Yuklandi

---
+++ JS/main.js
@@ -0,0 +1,21 @@
+/*undefined,boolean,object in JS*/
+
+/*let raqam = 45;
+let text = "JavaScript";
+let soz;
+let rost =true
+let arrayObject = [45,78,"matn","gap",true,false]
+let oddiyObject = {familya: "Tursunov", ism: "Shodmon", yoshi: 35};
+console.log(oddiyObject);*/
+
+function savatchaniTekshir(uzatilganMalumot){
+
+    if (savatcha.length === 0){
+        document.write("Savatchaning ichida" +uzatilganMalumot.length + "ta ma'lumot bor!");
+    }else{
+        document.write("Savatchaning ichida" +uzatilganMalumot.length + "dona ma'lumot bor!");
+    }
+}
+
+let container = [12,true,false];
+savatchaniTekshir(container);
